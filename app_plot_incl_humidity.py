e7dcd5b75f152498f40c7f4faedcc6a685331413
FabianRei <F.abi.an@gmx.de>
2024-04-22 11:33:08 +0200

    deactivate debug for more security

--- app_plot_incl_humidity.py
+++ app_plot_incl_humidity.py
@@ -204,5 +204,5 @@ def plot_humidity():
     return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
